615f5f9b586874bc37d120fe38e9ad8508986768
Erick Giraldo <erickgiraldo94@gmail.com>
2024-10-07 17:24:04 -0500

    fix: fixed an error in the authorization guard

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -6,37 +6,47 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { ApiTags } from '@nestjs/swagger';
+import { RolesGuard } from 'src/common/guards/roles.guard';
+import { JwtAuthGuard } from 'src/common/guards/authentication.guard';
+import { Roles } from 'src/common/decorators/roles.decorator';
 
 @ApiTags('Users')
 @Controller('users')
+@UseGuards(JwtAuthGuard, RolesGuard)
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @Roles(1)
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
 
+  @Roles(1, 2)
   @Get()
   findAll() {
     return this.usersService.findAll();
   }
 
+  @Roles(1, 2)
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.usersService.findOne(+id);
   }
 
+  @Roles(1, 2)
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update(+id, updateUserDto);
   }
 
+  @Roles(1, 2)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.usersService.remove(+id);
